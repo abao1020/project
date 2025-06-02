bca53248e24bd07eea797939e4948239f7ee0ce7
hsin22 <s11359026@gm2.nutn.edu.tw>
2025-06-02 16:48:45 +0800

    ft: foo

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void foo() {
+    cout << "It's foo!!" << endl;
+}
+
+void bar() {
+    
+}
+ 
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+    foo();
+    return 0;
+}
